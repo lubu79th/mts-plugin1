bcdc0c102e155427f135a8ab612554847b17d9d1
lubu79th <lubu79th@gmail.com>
2015-11-09 09:39:09 +0700

    Update plugin.php

--- src/amiexd/plugin.php
+++ src/amiexd/plugin.php
@@ -55,21 +55,7 @@ class plugin extends PluginBase implements Listener{
 		 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
-	/* private function saveFiles(){
-		 if(file_exists($this->getDataFolder()."config.yml")){
-			 if($this->getConfig()->get("version") !== $this->getDescription()->getVersion() or !$this->getConfig()->exists("version")){
-				 $this->getServer()->getLogger()->warning("An invalid configuration file for ".$this->getDescription()->getName()." was detected.");
-				 if($this->getConfig()->getNested("plugin.autoUpdate") === true){
-					 $this->saveResource("config.yml", true);
-					 $this->getServer()->getLogger()->warning("Successfully updated the configuration file for ".$this->getDescription()->getName()." to v".$this->getDescription()->getVersion().".");
-					}
-				}
-			}else{
-				 $this->saveDefaultConfig();
-				 $this->getServer()->getLogger()->warning("Remember to use a server restarter script, or else this plugin won't work properly.");
-			}
-			
-	}*/
+		
 	 private function registerAll(){
 		/***commandmap***/
 		 $this->getServer()->getCommandMap()->register("mts", new ClearLaggCommand($this));
